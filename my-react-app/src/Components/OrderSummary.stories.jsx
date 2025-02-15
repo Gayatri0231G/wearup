becff4b1dfd464f13f7abc25ef4a090f6f6339ec
Gouda12345 <gouda8051@gmail.com>
2025-02-15 10:18:27 +0530

    first commit

--- my-react-app/src/Components/OrderSummary.stories.jsx
+++ my-react-app/src/Components/OrderSummary.stories.jsx
@@ -1,15 +1,6 @@
-// export default {
-//     title: 'Components/OrderSummary',
-//     component: OrderSummary,
-//   };
-  
-//   const Template = (args) => <OrderSummary {...args} />;
-  
-//   export const Default = Template.bind({});
-//   Default.args = {};
 
 import React from "react";
-import OrderSummary from "./OrderSummary.jsx"; // Ensure the correct import path
+import OrderSummary from "./OrderSummary.jsx"; 
 
 export default {
   title: "Components/OrderSummary",
